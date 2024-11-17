c8ef52932990ff8ea0414c5d6bea90ff83398758
AArinova <aliya-arinova@yandex.ru>
2024-11-17 20:40:57 +0700

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -6,12 +6,19 @@ cursor.execute("""
     CREATE TABLE "Users" (
         "id"	INTEGER UNIQUE,
         "username"	TEXT NOT NULL,
-        "email "	TEXT NOT NULL,
+        "email"	TEXT NOT NULL,
         "age"	INTEGER,
         "balance"	INTEGER
     )
 """)
 connection.commit()
+
+for num in range(1, 11):
+    insert_str = '("User"' + str(num) + '", "example1@gmail.com", '+str(num*10) + ', 1000)'
+    requery_str = 'INSERT INTO "Users" ("username", "email", "age", "balance") VALUES ' + insert_str
+
+    cursor.execute(requery_str)
+    connection.commit()
+
 connection.close()
 
-#print("Hello")
